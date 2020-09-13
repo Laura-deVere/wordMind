8da1f2ef9d481b8b5145dd465d61575c0d0c8318
Laura-deVere <lauradevh@gmail.com>
2020-09-13 15:16:38 -0400

    testing gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import Nav from "./components/Nav";
 import "./App.scss";
 import Blurb from "./components/Blurb";
 
+const FAKE_API_KEY = process.env.REACT_APP_API_KEY;
+
+console.log(FAKE_API_KEY);
 function App() {
   return (
     <div className="App">

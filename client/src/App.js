092af73452c1ed5e0d759d425095cba609db5388
Laura-deVere <lauradevh@gmail.com>
2020-09-13 19:22:21 -0400

    added option to play audio of word pronunciation

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
 import Nav from "./components/Nav";
 import "./App.scss";
 import Blurb from "./components/Blurb";
 import Search from "./components/Search";
 import Word from "./components/Word";
 
-function App() {
+function App({ currentWord }) {
   return (
     <div className="App">
       <Nav />
       <Blurb />
       <Search />
-      <Word />
+      {currentWord.length ? <Word /> : null}
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return { currentWord: state.word };
+};
+export default connect(mapStateToProps)(App);
--- client/src/components/Word.js
+++ client/src/components/Word.js
@@ -1,13 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import useSound from "use-sound";
 
-const Word = () => {
+const URLHelper = (prs) => {
+  const alaphabet = "abcdefghijklmnopqrstuvwxyz";
+  let audio = prs[0].sound.audio;
+  let subDirectory;
+  let baseFilename = audio;
+  if (audio.startsWith("bix")) {
+    subDirectory = "bix";
+  } else if (audio.startsWith("gg")) {
+    subDirectory = "gg";
+  } else if (alaphabet.indexOf(audio) !== -1) {
+    subDirectory = "number";
+  } else {
+    subDirectory = audio[0];
+  }
+  let url = `https://media.merriam-webster.com/audio/prons/en/us/mp3/${subDirectory}/${baseFilename}.mp3`;
+  return url;
+};
+
+const Word = ({ isSignedIn, word }) => {
+  const [audioURL, setAudioURL] = useState("");
+  const [play, setPlay] = useSound(audioURL);
+  let newURL = URLHelper(word[0].hwi.prs);
+  let currentWord = word[0].meta.id;
+  currentWord = currentWord.replace(/[^a-zA-Z]/g, "");
+
+  const resetURLAndPlay = () => {
+    //   handle constant rerender, only update when ready
+    setAudioURL(newURL);
+    play();
+  };
+
   return (
     <div>
-      <h3>Hello</h3>
-      <i class="im im-megaphone"></i>
-      <i class="im im-star-o"></i>
+      <h3>{currentWord}</h3>
+      <button onClick={resetURLAndPlay}>
+        <i className="im im-megaphone"></i>
+      </button>
+      {isSignedIn ? <i className="im im-star-o"></i> : null}
+
+      <button>Learn More</button>
     </div>
   );
 };
 
-export default Word;
+const mapStateToProps = (state) => {
+  return { isSignedIn: state.auth.isSignedIn, word: state.word };
+};
+
+export default connect(mapStateToProps)(Word);
